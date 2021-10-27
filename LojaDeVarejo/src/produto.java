4d97be447226181a3cf16d05b61c04a7e797b31c
Matheus Reinheimer <79872570+reinheimermat@users.noreply.github.com>
2021-10-27 16:24:31 -0300

    Add files via upload

---
+++ LojaDeVarejo/src/produto.java
@@ -0,0 +1,7 @@
+public class produto {
+    public String nomeProduto;
+    public float valorProduto;
+    public String codProduto;
+    public String cupomProduto;
+    public String descriscaoProduto;
+}
---
+++ LojaDeVarejo/src/transportadora.java
@@ -0,0 +1,7 @@
+public class transportadora {
+    public String nomeTransportadora;
+    public String cnpj;
+    public String nomeMotorista;
+    public String enderecoEntrega;
+    public String codEntrega;
+}
